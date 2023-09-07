dbd78c0966ae44338a6d4757a5172db029b9d304
Mateo Astelarra <mateo@astelarra.com>
2023-09-07 16:15:42 -0300

    Arranged project folder and improved movement

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintWaypointName());
+    }
+
+    IEnumerator  PrintWaypointName()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
