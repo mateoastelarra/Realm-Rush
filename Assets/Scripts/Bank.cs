72dcef190574b855591e3b4ad6b98f16cb693641
Mateo Astelarra <mateo@astelarra.com>
2023-10-16 13:17:00 -0300

    Added gold UI

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,28 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
-    [SerializeField] int currentBalance;
+    [SerializeField] TextMeshProUGUI goldUI;
+
+    int currentBalance;
 
     public int CurrentBalance { get => currentBalance;}
 
     private void Awake()
     {
         currentBalance = startingBalance;
+        DisplayCurrentBalance();
     }
+
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        DisplayCurrentBalance();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        DisplayCurrentBalance();
+
         if (currentBalance < 0)
         {
             ReloadScene();
@@ -33,5 +39,10 @@ public class Bank : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    void DisplayCurrentBalance()
+    {
+        goldUI.text = "Gold: " + currentBalance;
     }
 }
