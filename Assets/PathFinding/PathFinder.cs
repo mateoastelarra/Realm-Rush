80be9b013fc4be5e2aed9cfb92f581333f7815f8
Mateo Astelarra <mateo@astelarra.com>
2023-10-19 13:56:02 -0300

    Started with Breadth First Search

--- Assets/PathFinding/PathFinder.cs
+++ Assets/PathFinding/PathFinder.cs
@@ -1,23 +1,33 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    [SerializeField] Node currentSearchNode;
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int endCoordinates;
+
+    Node startNode;
+    Node destinationNode;
+    Node currentSearchNode;
+
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
+
     Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
     GridManager gridManager;
-    Dictionary<Vector2Int, Node> grid;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
     private void Awake()
     {
         gridManager = FindObjectOfType<GridManager>();
         if (gridManager != null) { grid = gridManager.Grid; }
+
+        startNode = new Node(startCoordinates, true);
+        destinationNode = new Node(endCoordinates, true);
     }
     void Start()
     {
-        ExploreNeighbors();
+        BreadthFirstSearch();
     }
 
     private void ExploreNeighbors()
@@ -30,11 +40,34 @@ public class PathFinder : MonoBehaviour
             if (neighbor != null)
             {
                 neighbors.Add(neighbor);
-                // Remove Later, only for testing
-                neighbor.isExplored = true;
-                neighbor.isPath = true;
-                Debug.Log(neighbor.coordinates);
             }
         }
+
+        foreach (Node neighbor in neighbors)
+        {
+            if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+            {
+                frontier.Enqueue(neighbor);
+                reached.Add(neighbor.coordinates, neighbor);
+            }
+        }
+    }
+
+    void BreadthFirstSearch()
+    {
+        bool isRunning = true;
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+        while (frontier.Count > 0 && isRunning)
+        {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if (currentSearchNode.coordinates == endCoordinates)
+            {
+                isRunning = false;
+            }
+        }
+
     }
 }
