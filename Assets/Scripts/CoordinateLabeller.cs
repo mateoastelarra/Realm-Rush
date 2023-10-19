71a00cdd5b15a88f12ce02ad7c1ffabf20bc1373
Mateo Astelarra <mateo@astelarra.com>
2023-10-19 12:09:33 -0300

    Adapted coordinate labeller for debugging

--- Assets/Scripts/CoordinateLabeller.cs
+++ Assets/Scripts/CoordinateLabeller.cs
@@ -7,17 +7,19 @@ public class CoordinateLabeller : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.grey;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f,0.5f,0);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake() 
     {
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
         DisplayCurrentCoordinates();
-        waypoint = GetComponentInParent<Waypoint>();
+        gridManager = FindObjectOfType<GridManager>();
     }
 
     void Update()
@@ -34,14 +36,29 @@ public class CoordinateLabeller : MonoBehaviour
 
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }
-        else
-        {
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+        
+       if (!node.isWalkable)
+       {
             label.color = blockedColor;
-        }
+       }
+       else if (node.isPath)
+       {
+            label.color = pathColor;
+       }
+       else if (node.isExplored)
+       {
+            label.color = exploredColor;
+       }
+       else
+       {
+            label.color = defaultColor;
+       }
+       
     }
 
     void DisplayCurrentCoordinates()
