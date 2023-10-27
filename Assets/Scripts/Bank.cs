d13793e0ce38f50c2741e4069885f4205e6d3d81
Mateo Astelarra <mateo@astelarra.com>
2023-10-27 14:12:18 -0300

    Added UI for balance changes

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
     [SerializeField] TextMeshProUGUI goldUI;
+    [SerializeField] TextMeshProUGUI goldChangesUI;
 
     int currentBalance;
     public int CurrentBalance { get => currentBalance;}
@@ -20,12 +22,16 @@ public class Bank : MonoBehaviour
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        IEnumerator coroutine = DisplayGoldChanges(Mathf.Abs(amount));
+        StartCoroutine(coroutine);
         DisplayCurrentBalance();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        IEnumerator coroutine = DisplayGoldChanges(-Mathf.Abs(amount));
+        StartCoroutine(coroutine);
         DisplayCurrentBalance();
 
         if (currentBalance < 0)
@@ -43,5 +49,25 @@ public class Bank : MonoBehaviour
     void DisplayCurrentBalance()
     {
         goldUI.text = "Gold: " + currentBalance;
+    }
+
+    IEnumerator DisplayGoldChanges(int amount)
+    {
+        if (amount > 0)
+        {
+            goldChangesUI.text = "+ " + amount;
+            goldChangesUI.color = Color.green;
+        }
+        else if (amount < 0)
+        {
+            goldChangesUI.text = "- " + (-amount);
+            goldChangesUI.color = Color.red;
+        }
+
+        goldChangesUI.gameObject.SetActive(true);
+        
+        yield return new WaitForSeconds(1f);
+
+        goldChangesUI.gameObject.SetActive(false);
     }
 }
